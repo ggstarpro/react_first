9302b26adacdc470ca58ad60f57cc2ccebaba95e
GentaMorino <genta.mornio@gmail.com>
2022-02-28 04:11:14 +0900

    axios

---
+++ chapter2/section8/axios/src/App.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+function App() {
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res)=> {
+      console.log(res);
+    }).catch((err) => console.log(err));
+  }
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users?id=1").then((res)=> {
+      console.log(res);
+    }).catch((err) => console.log(err));
+  }
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+  );
+}
+
+export default App;
