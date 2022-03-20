05d68ea6870cca1258b3b89e3ce5d8d55ceaa5b9
GentaMorino <genta.morino@gmail.com>
2022-03-21 01:25:03 +0900

    Globalなスタイルを設定する

---
+++ chapter2/section12/my-app/src/theme/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+// Globalなスタイルを設定する
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "gray.100",
+        color: "gray.800"
+      }
+    }
+  }
+});
+
+export default theme;
