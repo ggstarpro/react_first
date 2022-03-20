a615fd979e7643c3d6596d8cc054b3e63c51cc6d
GentaMorino <genta.morino@gmail.com>
2022-03-21 03:30:07 +0900

    ルーティング

--- chapter2/section12/my-app/src/App.tsx
+++ chapter2/section12/my-app/src/App.tsx
@@ -1,12 +1,16 @@
 import { Button, ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './router/Router';
+
 import theme from './theme/theme';
 
 function App() {
   return (
     <ChakraProvider theme={ theme }>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>あああ</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
