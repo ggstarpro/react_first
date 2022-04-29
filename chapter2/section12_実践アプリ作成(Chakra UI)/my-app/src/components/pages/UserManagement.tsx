5165d1a577ceb5a1db714d8009447870986c10f5
GentaMorino <genta.morino@gmail.com>
2022-04-30 03:03:13 +0900

    77. ユーザー一覧画面の作成

--- chapter2/section12_実践アプリ作成(Chakra UI)/my-app/src/components/pages/UserManagement.tsx
+++ chapter2/section12_実践アプリ作成(Chakra UI)/my-app/src/components/pages/UserManagement.tsx
@@ -1,5 +1,13 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisums/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザ管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }} spacing="30px">
+      <WrapItem>
+        <UserCard imageUrl="https://source.unsplash.com/random" userName="ああ" fullName="フルネーム"/>
+      </WrapItem>
+    </Wrap>
+  );
 });
